673a9c694345afbdf00088a8476674b41c2ca5d8
Nguyễn Như Thịnh <nhuthinh0258@gmail.com>
2023-11-01 17:38:39 +0700

    Remove other projects from todo branch

--- first_app/lib/function.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:first_app/gradient_container.dart';
-
-final random = Random();
-var currentDiceRoll = 2;
-
--- quiz_app/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:quiz_app/quiz.dart';
-
-void main() {
-  runApp(const Quizz());
-}
-
